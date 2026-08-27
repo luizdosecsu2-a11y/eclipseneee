3452c015b8f729fc7be0dfb87faa47011db576c7
luizdosecsu2-a11y <luizdosecsu2@gmail.com>
2026-08-27 13:25:52 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import CasinoTracker from "./CasinoTracker.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <CasinoTracker />
+  </React.StrictMode>
 );
